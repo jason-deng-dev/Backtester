af5faadb7f8af1493f1099346ab11a9f9edfe190
jason-deng <jasondeng.dev@gmail.com>
2026-09-16 18:48:02 +0800

    refactor: plug in new Strategy implementation into main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "backtest.h"
 #include "datafeed.h"
 #include "strategy.h"
+#include "sizer.h"
+#include "signal.h"
+#include "riskmanager.h"
 #include "state.h"
 #include "analytics.h"
 
@@ -11,7 +14,11 @@ int main() {
   DataFeed df{};
   df.load("data/nvda_daily.csv");
 
-  naive_reversion_strategy rs{};
+  BuyHoldSignal signal;
+  FixedFractionalSizer sizer{1};
+  NotionalCapRiskManager riskManager{0.1, NotionalCapRiskManager::Policy::Clamp};
+
+  Strategy rs{signal, sizer, riskManager};
 
   State st(100000, 0);
 
