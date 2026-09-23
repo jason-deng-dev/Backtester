773ce46e853d9693b0dd3dac0424f187cb8c680b
jason-deng <jasondeng.dev@gmail.com>
2026-09-23 18:12:42 +0800

    feat: MonteCarlo::sampleTrade done

--- include/montecarlo.h
+++ include/montecarlo.h
@@ -1,17 +1,52 @@
 #pragma once
 
+#include "analytics.h"
 #include "rollingwindow.h"
 #include "state.h"
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
 #include <random>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 enum class Regime { WARMUP, VOLATILE, CALM };
 
+struct SampledTrade {
+  double pnl;
+  Regime regime;
+};
+
 class MonteCarlo {
 public:
-  MonteCarlo(std::mt19937 &gen) : gen_(gen) {}
+
+  void sampleNTrades(int n, int seed) {
+    for ()
+
+  }
+
+  void sampleTrade(int seed, const Analytics &analytics) {
+    // keep sampling until fill until reach
+    std::vector<SampledTrade> tradePath;
+    
+    const auto &positionRecords = analytics.getPositionRecords();
+    auto recordSize = positionRecords.size();
+
+    tradePath.reserve(recordSize);
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<std::size_t> dist(0,
+                                                    positionRecords.size() - 1);
+    while (tradePath.size() < recordSize) {
+      auto &position = positionRecords[dist(gen)];
+      tradePath.push_back({position.pnl, getRegime(position.openTime)});
+    }
+
+    // move to avoid unneeded copy
+    sampledTrades.push_back(std::move(tradePath));
+  }
 
   void classifyRegime(const State &state, double volPercentile = 0.75,
                       double calmPercentile = 0.6, int returnLookback = 20,
@@ -54,5 +89,5 @@ public:
 private:
   std::unordered_map<std::string, std::size_t> dateToIndexMap;
   std::vector<Regime> regimes;
-  std::mt19937 &gen_;
+  std::vector<std::vector<SampledTrade>> sampledTrades;
 };
