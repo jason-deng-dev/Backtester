ed839aead34238fe339fd0e4ebe390014e066c7d
jason-deng <jasondeng.dev@gmail.com>
2026-09-24 18:09:59 +0800

    refactor: move MonteCarlo implementation to cpp

--- include/montecarlo.h
+++ include/montecarlo.h
@@ -5,8 +5,11 @@
 #include "state.h"
 #include <algorithm>
 #include <cstddef>
+#include <cstdlib>
 #include <iterator>
+#include <limits>
 #include <random>
+#include <stdexcept>
 #include <string>
 #include <thread>
 #include <unordered_map>
@@ -16,99 +19,44 @@
 enum class Regime { WARMUP, VOLATILE, CALM };
 
 struct SampledTrade {
-  double pnl;
+  double fractionalReturn;
   Regime regime;
+};
+
+struct sampleOutcome {
+  double peak;
+  double trough;
+  double balance;
+  double maxDrawdown;
 };
 
 class MonteCarlo {
 public:
-  void sampleTrade(int seed, int i, const Analytics &analytics) {
-    // keep sampling until fill until reach
-    std::vector<SampledTrade> tradePath;
-
-    const auto &positionRecords = analytics.getPositionRecords();
-    auto recordSize = positionRecords.size();
-
-    tradePath.reserve(recordSize);
-
-    std::mt19937 gen(seed + i);
-    std::uniform_int_distribution<std::size_t> dist(0,
-                                                    positionRecords.size() - 1);
-    while (tradePath.size() < recordSize) {
-      auto &position = positionRecords[dist(gen)];
-      tradePath.push_back({position.pnl, getRegime(position.openTime)});
-    }
-
-    // move to avoid unneeded copy
-    sampledTrades[i] = std::move(tradePath);
-  }
-
+  void sampleTrade(int seed, int i, const Analytics &analytics);
 
   // ground-truth reference
-  void sampleTradesSerial(int n, int seed, const Analytics& analytics) {
-    sampledTrades.resize(n);
-    for (int i = 0; i < n; ++i) {
-      sampleTrade(seed, i, analytics);
-    }
-  }
+  void sampleTradesSerial(int n, int seed, const Analytics &analytics);
 
   // CPU multithreaded
-  void sampleTradesParallel(int n, int seed, const Analytics &analytics) {
-    sampledTrades.resize(n);
-    auto C = std::thread::hardware_concurrency();
-    std::vector<std::thread> threads;
-    for (unsigned t = 0; t < C; ++t) {
-      threads.emplace_back([&, t] {
-        for (size_t i = t; i < n; i += C) {
-          sampleTrade(seed, i, analytics);
-        }
-      });
-    }
-    for (auto &th : threads)
-      th.join();
-  }
+  void sampleTradesParallel(int n, int seed, const Analytics &analytics);
 
-  void sampleTradesCuda(int n, int seed, const Analytics &analytics);
+  // GPU
+  void sampleTradesCUDA(int n, int seed, const Analytics &analytics);
+
+  void computeAllPathStatSerial(int startingBalance);
+
+  void computePathStat(int n, int startingBalance);
 
   void classifyRegime(const State &state, double volPercentile = 0.75,
                       double calmPercentile = 0.6, int returnLookback = 20,
-                      int volLookback = 252) {
-
-    // need std dev
-    RollingWindow<double> returnWindow{returnLookback};
-
-    // need way to quickly calculate percentile from it
-    RollingWindow<double> volWindow{volLookback};
-
-    const auto &bars = state.getBarSnapshots();
-
-    Regime currState = Regime::CALM;
-    int index = 0;
-
-    for (auto &bar : bars) {
-      dateToIndexMap[bar.date] = index++;
-      const auto currVol = returnWindow.getStdDev();
-      const auto currHigh = volWindow.getPercentile(volPercentile);
-      const auto currLow = volWindow.getPercentile(calmPercentile);
-
-      if (!currVol || !currHigh) {
-        regimes.push_back(Regime::WARMUP);
-      } else {
-        if (currState == Regime::CALM && *currVol > *currHigh) {
-          currState = Regime::VOLATILE;
-        } else if (currState == Regime::VOLATILE && *currVol < *currLow) {
-          currState = Regime::CALM;
-        }
-        regimes.push_back(currState);
-      }
-    }
-  }
+                      int volLookback = 252);
 
   Regime getRegime(const std::string &date) const {
     return regimes.at(dateToIndexMap.at(date));
   }
 
 private:
+  std::vector<Outcome> outcomes;
   std::unordered_map<std::string, std::size_t> dateToIndexMap;
   std::vector<Regime> regimes;
   std::vector<std::vector<SampledTrade>> sampledTrades;
---
+++ src/montecarlo.cpp
@@ -0,0 +1,107 @@
+#include "montecarlo.h"
+
+void MonteCarlo::classifyRegime(const State &state, double volPercentile,
+                                double calmPercentile, int returnLookback,
+                                int volLookback) {
+
+  // need std dev
+  RollingWindow<double> returnWindow{returnLookback};
+
+  // need way to quickly calculate percentile from it
+  RollingWindow<double> volWindow{volLookback};
+
+  const auto &bars = state.getBarSnapshots();
+
+  Regime currState = Regime::CALM;
+  int index = 0;
+
+  for (auto &bar : bars) {
+    dateToIndexMap[bar.date] = index++;
+    const auto currVol = returnWindow.getStdDev();
+    const auto currHigh = volWindow.getPercentile(volPercentile);
+    const auto currLow = volWindow.getPercentile(calmPercentile);
+
+    if (!currVol || !currHigh) {
+      regimes.push_back(Regime::WARMUP);
+    } else {
+      if (currState == Regime::CALM && *currVol > *currHigh) {
+        currState = Regime::VOLATILE;
+      } else if (currState == Regime::VOLATILE && *currVol < *currLow) {
+        currState = Regime::CALM;
+      }
+      regimes.push_back(currState);
+    }
+  }
+}
+
+/*-----------------------Sample Trades-----------------------------*/
+
+void MonteCarlo::sampleTrade(int seed, int i, const Analytics &analytics) {
+  // keep sampling until fill until reach
+  std::vector<SampledTrade> tradePath;
+
+  const auto &positionRecords = analytics.getPositionRecords();
+  auto recordSize = positionRecords.size();
+
+  tradePath.reserve(recordSize);
+
+  std::mt19937 gen(seed + i);
+  std::uniform_int_distribution<std::size_t> dist(0,
+                                                  positionRecords.size() - 1);
+  while (tradePath.size() < recordSize) {
+    auto &position = positionRecords[dist(gen)];
+    tradePath.push_back({position.pnl / std::abs(position.entryNotional),
+                         getRegime(position.openTime)});
+  }
+
+  // move to avoid unneeded copy
+  sampledTrades[i] = std::move(tradePath);
+}
+
+void MonteCarlo::sampleTradesSerial(int n, int seed,
+                                    const Analytics &analytics) {
+  if (n == 0) {
+    throw std::logic_error("Can't sample 0 trades");
+  }
+  sampledTrades.resize(n);
+  for (int i = 0; i < n; ++i) {
+    sampleTrade(seed, i, analytics);
+  }
+}
+
+void MonteCarlo::sampleTradesParallel(int n, int seed,
+                                      const Analytics &analytics) {
+  if (n == 0) {
+    throw std::logic_error("Can't sample 0 trades");
+  }
+  sampledTrades.resize(n);
+  auto C = std::thread::hardware_concurrency();
+  std::vector<std::thread> threads;
+  for (unsigned t = 0; t < C; ++t) {
+    threads.emplace_back([&, t] {
+      for (size_t i = t; i < n; i += C) {
+        sampleTrade(seed, i, analytics);
+      }
+    });
+  }
+  for (auto &th : threads)
+    th.join();
+}
+
+/*-----------------------Compute Path Stats-----------------------------*/
+void MonteCarlo::computePathStat(int n, int startingBalance) {
+  double maxDrawDown{0};
+  double runningPeak{-std::numeric_limits<double>::infinity()};
+}
+
+void MonteCarlo::computeAllPathStatSerial(int startingBalance) {
+  int size = sampledTrades.size();
+  if (size == 0) {
+    throw std::logic_error("sampledTrades is empty");
+  }
+  outcomes.resize(size);
+
+  for (std::size_t i = 0; i < size; ++i) {
+    computePathStat(i, startingBalance);
+  }
+}
